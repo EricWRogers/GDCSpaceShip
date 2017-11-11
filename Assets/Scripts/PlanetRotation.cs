a5d9d8988c2d1a9e3cad0e38a5ef357738867c6c
Walyn <walyncurtis@gmail.com>
2017-11-11 06:29:20 -0600

    Made the planet in MainLevel a prefab, added a script that makes it rotate

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour {
+
+	public float rotateSpeed = 1f;
+	
+	void Update () {
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0,0,rotateSpeed));
+	}
+}
