c57af2108e5f79ffb2c18627512835cab0246da7
MelisaJ <MAJohnson4683@muleriders.saumag.edu>
2017-11-11 15:29:12 -0600

    Enemy health bar fixed

--- Assets/Scripts/EnemiesHealthBar.cs
+++ Assets/Scripts/EnemiesHealthBar.cs
@@ -1,25 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class EnemiesHealthBar : MonoBehaviour {
-    GameObject bar;
-    Enemy enemy;
-	// Use this for initialization
-	void Start () {
-        enemy = GameObject.Find("EnemyShip").GetComponent<Enemy>();
-       
-	}
+public class EnemiesHealthBar : MonoBehaviour
+{
+    public Image bar;
+    //Enemy enemy;
+    // Use this for initialization
+    void Start()
+    {
+        //enemy = GameObject.Find("EnemyShip").GetComponent<Enemy>();
+
+    }
     void FixedUpdate()
     {
-        bar.transform.localScale = new Vector3(Mathf.Clamp(((float)enemy.health / (float)enemy.maxHealth),
-                                                           0f, 1f), bar.transform.localScale.y,
-                                                     bar.transform.localScale.z);
+
     }
 
     // Update is called once per frame
-    void Update () {
-        bar = GameObject.FindGameObjectWithTag("EnemyHealthBar");
-
-	}
+    void Update()
+    {
+        bar.fillAmount = (float)GetComponent<Enemy>().health / (float)GetComponent<Enemy>().maxHealth;
+    }
 }
+
