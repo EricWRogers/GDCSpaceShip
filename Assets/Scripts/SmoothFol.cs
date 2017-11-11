2a4d32ba3156185f686d24f4e39af61dca91054e
Eric <yerick147083@gmail.com>
2017-11-11 13:50:48 -0600

    adding new script to EnemyShip.prefab

--- Assets/Scripts/SmoothFol.cs
+++ Assets/Scripts/SmoothFol.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SmoothFol : MonoBehaviour {
+	public GameObject plant = null;
+	public Transform target = null;
 
-	public Transform target;
-	public float distance = 3.0f;
-	public float height = 3.0f;
-	public float damping = 5.0f;
-	public bool smoothRotation = true;
-	public bool followBehind = true;
-	public float rotationDamping = 10.0f;
+	public float thrust;
+	public Rigidbody rb;
+
+	void Start () {
+		findPlant();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void findPlant(){
+		plant = GameObject.FindGameObjectWithTag("Plant");
+		target = plant.transform;
+	}
 
 	void Update () {
-		Vector3 wantedPosition;
-		if(followBehind)
-			wantedPosition = target.TransformPoint(0, height, -distance);
-		else
-			wantedPosition = target.TransformPoint(0, height, distance);
+		transform.LookAt (target);
+	}
 
-		transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
-
-		if (smoothRotation) {
-			Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
-			transform.rotation = Quaternion.Slerp (transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
-		}
-		else transform.LookAt (target, target.up);
+	void FixedUpdate()
+	{
+		rb.AddForce(transform.forward * thrust);
 	}
 }
