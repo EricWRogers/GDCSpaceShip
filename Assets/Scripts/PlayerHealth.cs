01c57faa07bf39a5fb10f2ddabd93998127c9f1e
Walyn <walyncurtis@gmail.com>
2017-11-11 15:56:13 -0600

    Player now has health, and can be damaged, and when at 0 health or less will die

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int maxHealth = 100;
+	int health;
+	public bool isAlive = false;
+
+	void Start () {
+		health = maxHealth;
+		isAlive = true;
+	}
+	
+	void Damage(int dmg) {
+		health -= dmg;
+		if (isAlive && health <= 0) {
+			Die();
+		}
+	}
+
+	void Die() {
+		//Handle whatever we want to do when the player dies
+	}
+}
