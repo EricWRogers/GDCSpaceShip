8c6d3ac36066563c4d5b71fe93d758ff0a483e09
Walyn <walyncurtis@gmail.com>
2017-11-11 10:08:51 -0600

    Renamed EnemyStats, enemies now drop a sphere that serves as a placeholder for upgrade pickups

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	int timer;
+	public int dur = 10;
+
+	public void RestartTimer() {timer = 0;}
+
+	void Update () {
+		timer++;
+		if (timer == dur*60) {
+			gameObject.SetActive(false);
+		}
+	}
+}
